513cfb50f946de5c617a736b3bdb91e3d553a505
codeurh24 <cci.corlouer@gmail.com>
2020-05-25 22:46:02 +0200

    random node color ok

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React from 'react';
 import Node from './components/Node';
 import NodeTemplate from './components/NodeTemplate'
 import './App.css';
+import CSS_COLOR_NAMES from './config/color'
 
 class App extends React.Component {
 
-  options = (e, obj, brotherIndex) => {
-    e.preventDefault();
-    console.log('App options', obj.state, 'brotherIndex', brotherIndex)
+  getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+  options = (obj, brotherIndex) => {
+    console.log('App options', obj.state, 'brotherIndex', brotherIndex);
+
+    const backColor = CSS_COLOR_NAMES[this.getRandomInt(CSS_COLOR_NAMES.length-1)];
+    obj.css().set(brotherIndex, {backgroundColor: backColor})
+    console.log('random colors', backColor)
   }
 
   render() {
--- src/components/Node.jsx
+++ src/components/Node.jsx
@@ -7,7 +7,7 @@ class Node extends React.Component {
         super(props);
 
         this.state = {
-            childs: []
+            childs:  []
         }
     }
 
@@ -27,6 +27,7 @@ class Node extends React.Component {
     css = () => {
         return {
             set: (childIndex, dataCSS) => {
+                console.log('css set ', childIndex, dataCSS)
                 //return new Promise((resolve) => {
                     //this.setState({ css: dataCSS }, resolve)
                     this.state.childs[childIndex] = {css: dataCSS};
@@ -36,36 +37,40 @@ class Node extends React.Component {
         }
     }
 
-    options = (e) => {
+    options = (e, brotherIndex) => {
         e.preventDefault();
-        this.props.options(e, this)
+        console.log('node options this.props.child', this.props.child);
+        this.props.options(this.props.parent, brotherIndex)
     }
 
     render() {
+        console.log('Render ', this.props.index+''+this.props.brotherIndex, this.state)
         return (
         (this.props.index < this.props.deepIndex) 
-            && (console.log('this.props.index', this.props.index) || true) 
-            && (this.props.index === 0 || this.props.childs.length) 
-            && <ul>
-                    <li className="node" style={this.props.css}>
-                        <div>
-                            <button onClick={this.options}>Options</button>
-                            <span>Node {this.props.index}{this.props.brotherIndex}</span>
-                            <button onClick={this.addNode}>+</button>
-                        </div>
-                    </li>
-                    {this.state.childs.map((elm,i) => {
-                        
-                        return <Node 
-                            index={this.props.index+1} 
-                            deepIndex={this.props.deepIndex} 
-                            childs={this.state.childs} 
-                            brotherIndex={i}
-                            css={elm.css}
-                            options={this.props.options}
-                        />;
-                    })}
-                </ul>
+            && (/*console.log('this.props', this.props, 'this.state', this.state) || */ true) 
+            &&   <>
+                        <ul>
+                            <li className="node" style={this.props.css}>
+                                <div>
+                                    <button onClick={(e) => {this.options(e, this.props.brotherIndex)}}>Options</button>
+                                    <span>Node {this.props.index}{this.props.brotherIndex}</span>
+                                    <button onClick={this.addNode}>+</button>
+                                </div>
+                            </li>
+                            {this.state.childs.map((elm,i) => {
+                                
+                                return <Node 
+                                    index={this.props.index+1} 
+                                    deepIndex={this.props.deepIndex} 
+                                    childs={this.state.childs} 
+                                    parent={this} 
+                                    brotherIndex={i}
+                                    css={elm.css}
+                                    options={this.props.options}
+                                />;
+                            })}
+                        </ul>
+                </>
         );
     }
 }
