1496e06061b189637c93187b5bcda80a41afe929
codeurh24 <cci.corlouer@gmail.com>
2020-05-27 14:06:17 +0200

    essais 1 rendu graph

--- src/components/Node.jsx
+++ src/components/Node.jsx
@@ -9,12 +9,17 @@ class Node extends React.Component {
             childs:  [],
             forceRender: false,
             css: (this.props.css === undefined) ? {} : this.props.css,
-            nodeName: (this.props.nodeName === undefined) ? 'Node' : this.props.nodeName
+            nodeName: (this.props.nodeName === undefined) ? 'Node' : this.props.nodeName,
+            weight:0
         }
+
+        // donne l'obj enfant au parent
+        if (this.props.parent)
+            this.props.parent.updateChild(this.props.brotherIndex , this);
     }
 
     shouldComponentUpdate(nextProps, nextState) {
-        console.log('shouldComponentUpdate', nextProps, nextState)
+        //console.log('shouldComponentUpdate', nextProps, nextState)
 
 
         // Le premier noeud par defaut détermine si le rendu est a faire.
@@ -36,14 +41,60 @@ class Node extends React.Component {
                 borderColor: 'red',
                 borderRadius: 10
             }, 
-            nodeName:'Node'
+            nodeName:'Node',
+            obj: {}
         })
 
         this.setState({
-            childs: this.state.childs
+            childs: this.state.childs,
+            //weight: this.state.childs.length
         }, () => {
-            console.log('addNode setState', this.state, 'deep', this.props.index)
+            //console.log('addNode setState', this.state, 'deep', this.props.index)
+            //if (this.props.parent) {
+                // if (this.state.childs.length > 1) 
+                    // this.props.parent.updateWeight(this.state.childs.length);
+                    this.updateWeight();
+            //}
         })
+    }
+
+    updateWeight() {
+        // console.log('updateWeight index(',this.props.index,')', this.state.weight, this.state.childs.length)
+        //console.log('updateWeight index(',this.props.index,')', this.state.childs)
+
+        let virtualChilds = 0;
+        this.state.childs.map((elm,i) => {
+            //console.log('Calcule: ', elm.obj.state.childs.length);
+            if (elm.obj.state.childs.length === 0) {
+                virtualChilds++;
+                //console.log('virtualChilds = elm.obj.state.childs.length: ', virtualChilds,'++', elm.obj.state.childs.length);
+            }else {
+                virtualChilds += elm.obj.state.childs.length;
+                //console.log('virtualChilds = elm.obj.state.childs.length: ', virtualChilds,'+=', elm.obj.state.childs.length);
+            }
+        })
+        console.log('virtualChilds index(',this.props.index,')', virtualChilds);
+
+
+        this.setState({
+            weight: virtualChilds
+        })
+
+        if (this.props.parent) this.props.parent.updateWeight();
+
+
+        // (this.state.childs-1) le parent recalcule ces nombres d'enfant sauf celui qui
+        // informe des ces enfants car il ne prend plus la place de 1.
+        // +weightChild le parent remplace le 1 par le nouveau nombre d'enfant que celui ci contient.
+
+        // this.setState({weight: (this.state.childs.length-1)+weightChild}, () => {
+        //     if (this.props.parent) this.props.parent.updateWeight(this.state.weight);
+        // })
+    }
+
+    // recupere l'enfant apres ça création
+    updateChild(brotherIndex, child){
+        this.state.childs[brotherIndex].obj = child;
     }
 
     setName = (name) => {
@@ -65,40 +116,56 @@ class Node extends React.Component {
     }
 
     render() {
-        console.log('Render ', this.props.index+''+this.props.brotherIndex, this.state);
+        //console.log('Render ', this.props.index+''+this.props.brotherIndex, this.state);
 
         // let nodeName = 'Node';
         // if (this.props.nodeName !== undefined) {
         //     nodeName = this.props.nodeName;
         // }
 
+        let h = 47*this.state.childs.length;
+        this.state.childs.map((elm,i) => {
+            h += (i) ? 47 : 0
+        })
+        
+
         return (
         (this.props.index < this.props.deepIndex) 
             && (/*console.log('this.props', this.props, 'this.state', this.state) || */ true) 
             &&   <>
-                        <ul>
-                            <li className="node" style={this.state.css}>
-                                <div>
-                                    <span className="btn"><button onClick={(e) => {this.options(e, this.props.brotherIndex)}} className="options">Options</button></span>
-                                    <span className="title">{this.state.nodeName} {this.props.index}{this.props.brotherIndex}</span>
-                                    <button className="btn-add-node" onClick={this.addNode}>+</button>
-                                </div>
-                            </li>
-                            {this.state.childs.map((elm,i) => {
-                                
-                                return <Node 
-                                    key={i}
-                                    index={this.props.index+1} 
-                                    deepIndex={this.props.deepIndex} 
-                                    childs={this.state.childs} 
-                                    parent={this} 
-                                    brotherIndex={i}
-                                    css={elm.css}
-                                    nodeName={elm.nodeName}
-                                    options={this.props.options}
-                                />;
-                            })}
-                        </ul>
+                    <div className="ul" style={this.props.ulCSS}>
+                        <div className="node" style={this.state.css}>
+                            <div>
+                                <span className="btn">
+                                    <button onClick={(e) => {this.options(e, this.props.brotherIndex)}} className="options">Options</button>
+                                </span>
+                                <span className="title">{this.state.nodeName} {this.props.index}.{this.props.brotherIndex}_{this.state.weight}</span>
+                                <button className="btn-add-node" onClick={this.addNode}>+</button>
+                            </div>
+                        </div>
+                        {this.state.childs.length && <div className="links" style={{height: h}}></div> }
+                        {this.state.childs.map((elm,i) => {
+
+                            let top = 0;
+                            if (i===0) top = -h; 
+
+
+                            //console.log('render node', 'marginTop', top)
+                            return <Node 
+                                key={i}
+                                index={this.props.index+1} 
+                                deepIndex={this.props.deepIndex} 
+                                childs={this.state.childs} 
+                                parent={this} 
+                                brotherIndex={i}
+                                css={elm.css}
+                                nodeName={elm.nodeName}
+                                options={this.props.options}
+                                updateWeight={this.updateWeight}
+                                ulCSS={{marginTop: top}}
+                            />;
+                        })}
+                    </div>
                 </>
         );
     }
